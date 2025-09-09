7f025facac1bfa2d9a5bfe4dbf2eea037e2a602f
Harita M U <162999045+Harita27@users.noreply.github.com>
2025-09-09 18:33:35 +0530

    Time: 7 ms (54.57%), Space: 46 MB (15%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.java
@@ -1,34 +1,29 @@
 class Solution {
-    public int numSubmat(int[][] mat) {
-        int r = mat.length, c = mat[0].length;
-        int[] h = new int[c];
-        int ans = 0;
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                h[j] = (mat[i][j] == 0) ? 0 : h[j] + 1;
+    private static int countone(int[] hist){
+        int res=0;
+        for(int i=0;i<hist.length;i++){
+            int minh=hist[i];
+            for(int j=i;j>=0;j--){
+                minh=Math.min(hist[j],minh);
+                res+=minh;
             }
-            ans += count(h);
-        }
-        return ans;
-    }
-
-    private int count(int[] h) {
-        int n = h.length, res = 0;
-        int[] sum = new int[n];
-        Deque<Integer> st = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && h[st.peek()] >= h[i]) st.pop();
-            if (!st.isEmpty()) {
-                int p = st.peek();
-                sum[i] = sum[p] + h[i] * (i - p);
-            } else {
-                sum[i] = h[i] * (i + 1);
-            }
-            st.push(i);
-            res += sum[i];
         }
         return res;
+    }
+    public int numSubmat(int[][] mat) {
+        int R=mat.length;
+        int C=mat[0].length;
+        int[] hist=new int[C];
+        int total=0;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(mat[i][j]==0){
+                    hist[j]=0;
+                }
+                else hist[j]+=1;
+            }
+            total+=countone(hist);
+        }
+        return total;
     }
 }
