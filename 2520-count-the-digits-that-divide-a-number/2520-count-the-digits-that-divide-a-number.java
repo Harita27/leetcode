80dbdd75fe184cf395cbfe19e6fc99b678024a5e
Harita M U <162999045+Harita27@users.noreply.github.com>
2025-12-06 18:26:34 +0530

    Time: 0 ms (100%), Space: 41.8 MB (45%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.java
@@ -2,18 +2,12 @@ class Solution {
     public int countDigits(int num) {
         int n=num;
         int c=0;
-        PriorityQueue<Integer> minheap=new PriorityQueue<>();
         while(n>0){
             int temp=n%10;
-            minheap.add(temp);
-            n=n/10;
-        }
-        while(!minheap.isEmpty()){
-            int r=minheap.poll();
-            if(num%r==0){
+            if(num%temp==0){
                 c++;
             }
-            else continue;
+            n=n/10;
         }
         return c;
     }
