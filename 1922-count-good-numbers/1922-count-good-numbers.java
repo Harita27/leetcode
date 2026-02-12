6f65c8b43d06f040dff12bc1e75fa4a007bb60f7
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-02-12 21:06:04 +0530

    Time: 0 ms (100%), Space: 42.5 MB (14.52%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    static final long MOD = 1000000007;
+
+    public int countGoodNumbers(long n) {
+
+        long evenCount = (n + 1) / 2;
+        long oddCount = n / 2;   
+
+        long evenPart = power(5, evenCount);
+        long oddPart = power(4, oddCount);
+
+        return (int)((evenPart * oddPart) % MOD);
+    }
+    private long power(long base, long exp) {
+        long result = 1;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % MOD;
+            }
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+
+        return result;
+    }
+}
