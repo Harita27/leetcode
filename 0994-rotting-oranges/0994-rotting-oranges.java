26ca0dbba4298d3d63863e3d8be4a50865086d79
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-03-23 09:01:54 +0530

    Time: 2 ms (90.48%), Space: 44.4 MB (34.56%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -5,42 +5,43 @@ class Solution {
         int C=grid[0].length;
         Queue<int[]> que=new LinkedList();
         boolean[][] visited=new boolean[R][C];
-        int oranges=0;int count=-1;
+        int oranges=0;
         for(int row=0;row<R;row++){
             for(int col=0;col<C;col++){
-                if(grid[row][col]!=0){
                     if(grid[row][col]==2){
                         visited[row][col]=true;
-                        que.add(new int[]{row,col});
+                        que.add(new int[]{row,col,0});
                     }
-                    oranges++;
-                }
+                    if(grid[row][col]==1)oranges++;
             }
         }
         if(oranges==0){
             return 0;
         }
         int[][] diff=new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
+        int time=0;
         while(!que.isEmpty()){
             int size=que.size();
-            count++;
             for(int i=0;i<size;i++){
                 int[] cell=que.poll();
-                oranges--;
                 int row=cell[0];
                 int col=cell[1];
+                int t=cell[2];
                 for(int ind=0;ind<4;ind++){
                     int adjR=row+diff[ind][0];
                     int adjC=col+diff[ind][1];
                     if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && !visited[adjR][adjC] && grid[adjR][adjC]==1){
                         visited[adjR][adjC]=true;
-                        que.add(new int[]{adjR,adjC});
+                        que.add(new int[]{adjR,adjC,t+1});
+                        time=Math.max(time,t+1);
+                        oranges--;
+
                     }
                 }
             }
         }
         if(oranges>0) return -1;
-        else return count;
+        else return time;
 
                 
     }
