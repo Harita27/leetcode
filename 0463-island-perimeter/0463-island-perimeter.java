470dfd4096bfbe203e6ae9ca4fb23edc27aeef47
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-03-23 13:10:43 +0530

    Time: 4 ms (99.97%), Space: 47.1 MB (88.54%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,33 +1,18 @@
-class Solution {
-    int diff[][]={{-1,0},{1,0},{0,1},{0,-1}};
-    int p=0;
-    public void dfs(int[][] grid,int R,int C, int row, int col){
-         if(row<0 || row>=R || col<0 || col>=C){
-            p++; return;
-         }
-         if(grid[row][col] == 0){
-            p++;
-            return;
-        }
-         if(grid[row][col]==-1) return;
-         grid[row][col]=-1;
-
-
-        for(int i=0;i<4;i++){
-            int adjR=row+diff[i][0];
-            int adjC=col+diff[i][1];
-            dfs(grid,R,C,adjR,adjC);
-            
-        }
-    }
-
+class Solution{
     public int islandPerimeter(int[][] grid) {
         int R=grid.length;
         int C=grid[0].length;
+        int p=0;
         for(int i=0;i<R;i++){
             for(int j=0;j<C;j++){
                 if(grid[i][j]==1){
-                    dfs(grid,R,C,i,j);
+                    p+=4;
+                    if(i>0 && grid[i-1][j]==1){
+                        p-=2;
+                    }
+                    if(j>0 && grid[i][j-1]==1){
+                        p-=2;
+                    }
                 }
             }
         }
