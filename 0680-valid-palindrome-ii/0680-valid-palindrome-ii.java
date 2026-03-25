ad1a325fe436d251220d0ccfc73927c3658fc9d7
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-03-25 08:36:38 +0530

    Time: 4 ms (99.67%), Space: 47.2 MB (98.63%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,22 +1,26 @@
 class Solution {
-    private boolean isPalindrome(String s,int l,int r) {
-        while (l<r) {
-            if (s.charAt(l)!=s.charAt(r)) {
-                return false;
+    public boolean skipPossible(String s,int l,int r){
+        while(l<r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
             }
-            l++;
-            r--;
+            else return false;
         }
         return true;
+
     }
     public boolean validPalindrome(String s) {
-        int l=0,r=s.length()-1;
-         while(l<r) {
-            if (s.charAt(l)!=s.charAt(r)) {
-                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+        int l=0;
+        int r=s.length()-1;
+        while(l<r){
+            if(s.charAt(l)==s.charAt(r)){
+                l++;
+                r--;
             }
-            l++;
-            r--;
+            else{
+                return skipPossible(s,l+1,r)||skipPossible(s,l,r-1);
+            }
         }
         return true;
     }
