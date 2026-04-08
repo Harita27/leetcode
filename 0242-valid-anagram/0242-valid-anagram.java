1457212ea6f5e347f5649f05ce7dd37f3f365336
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-04-08 06:45:39 +0530

    Time: 5 ms (61.36%), Space: 44 MB (98.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,15 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        char[] ch1 = s.toCharArray();
-        char[] ch2 = t.toCharArray();
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-
-        return Arrays.equals(ch1, ch2);
+        int[] ch=new int[26];
+        for(int i=0;i<s.length();i++){
+            ch[s.charAt(i)-'a']++;
+            ch[t.charAt(i)-'a']--;
+        }
+        for(int c:ch){
+            if(c>0) return false;
+        }
+        return true;
 
     }
 }
