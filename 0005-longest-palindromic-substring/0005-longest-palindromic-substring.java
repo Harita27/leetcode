75fc4389df3a9054d2d637ce08b31f5bb737caa9
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-04-12 19:56:28 +0530

    Time: 19 ms (41.06%), Space: 47 MB (25.15%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,20 @@
 class Solution {
-    public String longestPalindrome(String word) {
-        int n = word.length();
-        boolean[][] dp = new boolean[n][n];
-        int start = 0;
-        int maxLength = 1;
-        // All substrings of length 1 are palindromes
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
+    public String longestPalindrome(String s) {
+        String ans="";
+        for(int i=0;i<s.length();i++){
+            String a=expand(s,i,i);
+            String b=expand(s,i,i+1);
+            if(a.length()>ans.length()) ans=a;
+            if(b.length()>ans.length()) ans=b;
         }
-        // Check substrings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (word.charAt(i) == word.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                start = i;
-                maxLength = 2;
-            }
+        return ans;
+        
+    }
+    public String expand(String s,int left,int right){
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
         }
-        // Check substrings of length 3 and more
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
-                if (word.charAt(i) == word.charAt(j) && dp[i + 1][j - 1]) {
-                    dp[i][j] = true;
-                    if (len > maxLength) {
-                        start = i;
-                        maxLength = len;
-                    }
-                }
-            }
-        }
-
-        return word.substring(start, start + maxLength);
+        return s.substring(left+1,right);
     }
 }
