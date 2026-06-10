02383deea2ee9e871b31b200d4e9b74e8363b02d
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-06-10 11:36:51 +0530

    Time: 78 ms (14.02%), Space: 76.6 MB (5.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,13 +1,33 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> maxheap=new PriorityQueue<>(Collections.reverseOrder());
-        for(int num:nums){
-            maxheap.add(num);
+    public int maxh(int[] nums,int k){
+        PriorityQueue<Integer> maxheap= new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            maxheap.add(nums[i]);
         }
-        int ans=-1;
+        int ki=0;
         for(int i=0;i<k;i++){
-            ans=maxheap.poll();
+            ki=maxheap.poll();
+        } 
+        return ki;
+    }
+    public int minh(int[] nums,int k){
+        PriorityQueue<Integer> minheap= new PriorityQueue<>();
+        for (int num: nums){
+            if(minheap.size()<k || minheap.peek()<num ){
+                minheap.add(num);
+            }
+            if(minheap.size()>k){
+                minheap.poll();
+            }
         }
-        return ans;
+        return minheap.poll();
+    }
+    public int findKthLargest(int[] nums, int k) {
+        if(k>nums.length/2){
+            return minh(nums,k);
+        }
+        else{
+            return maxh(nums,k);
+        }
     }
 }
