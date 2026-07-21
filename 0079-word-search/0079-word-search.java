949b371e7cbe77a2da291642b767af260d1cd245
Harita M U <162999045+Harita27@users.noreply.github.com>
2026-07-21 19:15:36 +0530

    Time: 155 ms (29.94%), Space: 43.1 MB (44.05%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,38 +1,36 @@
 class Solution {
-    int[][] diff=new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-    private boolean dfs(int R, int C,int row, int col,char[][] board,String word,boolean[][] visited,int wi){
-        int N=word.length();
-        if(wi==word.length()) return true;
+    int[][] diff=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+    public boolean dfs(char[][] board, String word,int R, int C, int row, int col, boolean[][] visited,int ind){
         visited[row][col]=true;
+        if(ind==word.length()) return true;
         for(int i=0;i<4;i++){
             int adjR=row+diff[i][0];
             int adjC=col+diff[i][1];
-            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C){
-                if(board[adjR][adjC]==word.charAt(wi)&& !visited[adjR][adjC])
-                {
-                    if (dfs(R,C,adjR,adjC,board,word,visited,wi+1)) return true;
+            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C  && !visited[adjR][adjC]){
+                if(board[adjR][adjC]==word.charAt(ind)){
+                    boolean fl=dfs(board,word,R,C,adjR,adjC,visited,ind+1);
+                    if(fl) return true;
                 }
             }
-
         }
         visited[row][col]=false;
         return false;
-    }
+    } 
     public boolean exist(char[][] board, String word) {
         int R=board.length;
         int C=board[0].length;
+        boolean fl=false;
+        boolean[][] visited=new boolean[R][C];
+        int ind=0;
         for(int row=0;row<R;row++){
             for(int col=0;col<C;col++){
                 if(board[row][col]==word.charAt(0)){
-                    boolean[][] visited=new boolean[R][C];
-                    boolean found=dfs(R,C,row,col,board,word,visited,1);
-                    if (found){
-                        return true;
-                    }
+                    visited[row][col]=true;
+                    fl=dfs(board,word,R,C,row,col,visited,ind+1);
+                    if(fl) return true;
                 }
             }
         }
-        return false;
-
+        return fl;
     }
 }
